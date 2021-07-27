e42f5937d6d9355a96e3750fc73400d387863226
Renato Bueno <renatosapa@gmail.com>
2021-07-27 20:39:44 -0300

    added function increment and decrement

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,21 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const totalProducts = products.reduce((acc, product) => {
+      const subTotalProducts = product.price * product.quantity;
+      return acc + subTotalProducts;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(totalProducts);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalProducts = products.reduce((acc, product) => {
+      const quantitylProducts = product.quantity;
+      return acc + quantitylProducts;
+    }, 0);
 
-    return 0;
+    return totalProducts;
   }, [products]);
 
   return (
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,29 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalProducts = products.reduce((acc, product) => {
+      const subTotalProducts = product.price * product.quantity;
+      return acc + subTotalProducts;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(totalProducts);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalProducts = products.reduce((acc, product) => {
+      const quantitylProducts = product.quantity;
+      return acc + quantitylProducts;
+    }, 0);
 
-    return 0;
+    return totalProducts;
   }, [products]);
 
   return (
